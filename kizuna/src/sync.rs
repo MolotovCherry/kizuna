fb2b84dc9d39568d76ae5c74f8242a354d843f4a
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2025-10-26 17:02:13 -0700

    Add Windows mutex

--- kizuna/src/sync.rs
+++ kizuna/src/sync.rs
@@ -1,29 +1,5 @@
-/// Make any T Send+Sync
-pub struct Sendable<T: ?Sized>(T);
+mod mutex;
+mod sendable;
 
-unsafe impl<T> Send for Sendable<T> {}
-unsafe impl<T> Sync for Sendable<T> {}
-
-impl<T> Sendable<T> {
-    pub fn new(t: T) -> Self {
-        Self(t)
-    }
-
-    /// # Safety
-    /// All safety requirements for `T` apply
-    pub unsafe fn as_ref(&self) -> &T {
-        &self.0
-    }
-
-    /// # Safety
-    /// All safety requirements for `T` apply
-    pub unsafe fn as_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-
-    /// # Safety
-    /// All safety requirements for `T` apply
-    pub unsafe fn into_inner(self) -> T {
-        self.0
-    }
-}
+pub use mutex::Mutex;
+pub use sendable::Sendable;
